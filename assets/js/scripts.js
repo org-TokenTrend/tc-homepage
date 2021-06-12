44b4cc98aa0014e5a86adb1835e51a57cfdf52e8
dosjia <dosjia@163.com>
2021-06-12 18:35:38 +0800

    add multi language

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,287 +3,282 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
     */
-    (function($) {
-    "use strict"; // Start of use strict
-    
-    // Smooth scrolling using jQuery easing
-    var chart = Highcharts.chart('container1', {
-    	chart: {
-    		type: 'pie',
-    		options3d: {
-    			enabled: true,
-    			alpha: 45,
-    			beta: 0
-    		}
-    	},
-    	title: {
-    		text: 'TokenTrend'
-    	},
-    	tooltip: {
-    		pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    	},
-    	plotOptions: {
-    		pie: {
-    			allowPointSelect: true,
-    			cursor: 'pointer',
-    			depth: 40,
-    			dataLabels: {
-    				enabled: true,
-    				format: '{point.name}'
-    			}
-    		}
-    	},
-    	series: [{
-    		type: 'pie',
-    		name: 'TT',
-    		data: [
-    			['Team 20%',   20.0],
-    			['Community Building 5%',  5.0],
-    			{
-    				name: 'Environmental Protection 5%',
-    				y: 5.0,
-    				sliced: true,
-    				selected: true
-    			},
-    			['Game/Mining Reward 25%',    25.0],
-    			['Ecological Construction 10%',   10.0],
-    			['Fundation 10%',   10.0],
-    			['Consultant 5%',   5.0],
-    			['Token Sale 20%',   20.0]
-    		]
-    	}]
-    });
+(function ($) {
+  "use strict"; // Start of use strict
 
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 71)
-          }, 1000, "easeInOutExpo");
-          return false;
+  // Smooth scrolling using jQuery easing
+  var chart = Highcharts.chart('container1', {
+    chart: {
+      type: 'pie',
+      options3d: {
+        enabled: true,
+        alpha: 45,
+        beta: 0
+      }
+    },
+    title: {
+      text: 'TokenTrend'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        depth: 40,
+        dataLabels: {
+          enabled: true,
+          format: '{point.name}'
         }
       }
-    });
-    
-    // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-    
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-    
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 80
-    });
-    
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-    
-    // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-      });
-    });
-    
-  })(jQuery); // End of use strict
+    },
+    series: [{
+      type: 'pie',
+      name: 'TT',
+      data: [
+        ['Team 20%', 20.0],
+        ['Community Building 5%', 5.0],
+        {
+          name: 'Environmental Protection 5%',
+          y: 5.0,
+          sliced: true,
+          selected: true
+        },
+        ['Game/Mining Reward 25%', 25.0],
+        ['Ecological Construction 10%', 10.0],
+        ['Fundation 10%', 10.0],
+        ['Consultant 5%', 5.0],
+        ['Token Sale 20%', 20.0]
+      ]
+    }]
+  });
 
-  function initParticles(){
-    particlesJS("particles", {
-      "particles": {
-        "number": {
-          "value": 30,
-          "density": {
-            "enable": true,
-            "value_area": 800
-          }
-        },
-        "color": {
-          "value": "#bfbbae"
-        },
-        "shape": {
-          "type": "image",
-          "stroke": {
-            "width": 0,
-            "color": "#000000"
-          },
-          "polygon": {
-            "nb_sides": 5
-          },
-        },
-        "opacity": {
-          "value": 0.5,
-          "random": false,
-          "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
-          }
-        },
-        "size": {
-          "value": 10,
-          "random": true,
-          "anim": {
-            "enable": false,
-            "speed": 40,
-            "size_min": 0.1,
-            "sync": false
-          }
-        },
-        "line_linked": {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 71)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 80
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Floating label headings for the contact form
+  $(function () {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function () {
+      $(this).removeClass("floating-label-form-group-with-focus");
+    });
+  });
+
+})(jQuery); // End of use strict
+
+function initParticles() {
+  particlesJS("particles", {
+    "particles": {
+      "number": {
+        "value": 30,
+        "density": {
           "enable": true,
-          "distance": 150,
-          "color": "#bfbbae",
-          "opacity": 0.4,
-          "width": 1
-        },
-        "move": {
-          "enable": true,
-          "speed": 6,
-          "direction": "none",
-          "random": false,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": false,
-          "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-          }
+          "value_area": 800
         }
       },
-      "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-          "onhover": {
-            "enable": true,
-            "mode": "grab"
-          },
-          "onclick": {
-            "enable": true,
-            "mode": "push"
-          },
-          "resize": true
+      "color": {
+        "value": "#bfbbae"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
         },
-        "modes": {
-          "grab": {
-            "distance": 140,
-            "line_linked": {
-              "opacity": 1
-            }
-          },
-          "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 8,
-            "speed": 3
-          },
-          "repulse": {
-            "distance": 200,
-            "duration": 0.4
-          },
-          "push": {
-            "particles_nb": 4
-          },
-          "remove": {
-            "particles_nb": 2
-          }
+        "polygon": {
+          "nb_sides": 5
+        },
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
         }
       },
-      "retina_detect": true
-    });
-  }
+      "size": {
+        "value": 10,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#bfbbae",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+}
 
 
-  function enablei18n(setLang) {
-   'use strict';
-   var i18n = $.i18n();
-   var lang = 'en';
-
-   if(setLang == false){
-     var sessionLang = sessionStorage.getItem('dgc-lang');
-     if(typeof sessionLang == "undefined" || sessionLang == null || sessionLang == "null" || sessionLang == ""){
-       var userLang = navigator.language || navigator.userLanguage; 
-       if(typeof userLang == "undefined" || userLang == ""){
+function enablei18n(setLang) {
+  'use strict';
+  var i18n = $.i18n();
+  var lang = 'en';
+  console.log("test");
+  if (setLang == false) {
+    var sessionLang = sessionStorage.getItem('trend-lang');
+    if (typeof sessionLang == "undefined" || sessionLang == null || sessionLang == "null" || sessionLang == "") {
+      var userLang = navigator.language || navigator.userLanguage;
+      if (typeof userLang == "undefined" || userLang == "") {
         userLang = 'en-EN';
       }
-      if(userLang.indexOf('-')){
+      if (userLang.indexOf('-')) {
         lang = userLang.split('-')[0];
       }
     }
-    else{
+    else {
       lang = sessionLang;
     }
   }
-  else{
+  else {
     lang = setLang;
   }
 
-  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it' && lang != 'hi' && lang != 'id' && lang!='mr' && lang != 'tr'){
+  if (lang != 'en' && lang != 'ch') {
     lang = 'en';
   }
-  sessionStorage.setItem('dgc-lang', lang);
-    
+  sessionStorage.setItem('trend-lang', lang);
+
   i18n.locale = lang;
-  
+
   i18n.load('assets/i18n', i18n.locale).done(
-    function() {
-     $('[data-i18n]').each(function(index) {
-      $(this).i18n();
-      $(this).html($.i18n($(this).attr('data-i18n')));
-    } );
-   } );
+    function () {
+      $('[data-i18n]').each(function (index) {
+        $(this).i18n();
+        $(this).html($.i18n($(this).attr('data-i18n')));
+      });
+    });
 }
 // Enable debug
 $.i18n.debug = false;
 
-$(document).ready(function(){
+$(document).ready(function () {
 
   enablei18n(false);
-  initParticles();
-  
+  // initParticles();
 
-  $(document).on("click", ".wallet-button", function(){
-    $(".wallet-guide-detail").hide();
-    $($(this).attr('data-target')).fadeIn();
-  });
-  $(document).on('click', '.select-lang', function(){
+  $(document).on('click', '.select-lang', function () {
     enablei18n($(this).attr('data-lang'));
     $(".scroll-to-top a").trigger('click');
   });
 
-//  $('#player').play();
+  //  $('#player').play();
 
 });
-function play_v1(v){
-        v.play()
-    }
+function play_v1(v) {
+  v.play()
+}
 
